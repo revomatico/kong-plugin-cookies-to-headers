af8f1582e554cce1867fef180c14f4c1e04d5f95
Cristian Chiru <cristi.chiru@gmail.com>
2023-03-08 09:16:12 +0200

    Update for compatibility with kong 3.x

--- schema.lua
+++ schema.lua
@@ -1,7 +1,26 @@
+local typedefs = require "kong.db.schema.typedefs"
+
 return {
-  no_consumer = true,
+  name = "cookies-to-headers",
   fields = {
-   cookie_name = { type = "string", default="SESSION" },
-   header_name = { type = "string", default="Authorization" }
+    {
+      -- this plugin will only be applied to Services or Routes
+      consumer = typedefs.no_consumer
+    },
+    {
+      config = {
+        type = "record",
+        fields = {
+          cookie_name = {
+            type = "string",
+            default = "SESSION"
+          },
+          header_name = {
+            type = "string",
+            default = "Authorization"
+          }
+        }
+      }
+    }
   }
 }
